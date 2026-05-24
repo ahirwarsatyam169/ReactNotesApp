9b0adf389355d43ef828cc7b5c03034993339d45
ahirwarsatyam169 <ahirwarsatyam169@gmail.com>
2026-05-24 11:33:34 +0530

    Add new note functionality and UI updates
    Added a new function to create a new note, including a button in the menu to trigger it. Updated styles for various elements for better UI consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -53,12 +53,18 @@ function App() {
     setContent(foundNote.content);
   };
 
+  //New Note button function
+  const newNote = () =>{
+    setTitle('');
+    setContent('');
+  }
+
   return (
     <>
       <div className="flex justify-center h-screen w-screen items-center bg-gradient-to-r from-amber-100 to-amber-100">
         <div className="shadow-xl border border-sm  w-[90%] h-[90%] justify-center bg-gradient-to-r from-amber-200 to-yellow-200 relative">
           {/* Header */}
-          <div className="shadow-sm ml-2 mt-2 border-none rounded h-[60px] w-[99%] items-center flex p-2 text-xl bg-gradient-to-l from-amber-100 to-yellow-100 justify-between">
+          <div className="text-black/75 shadow-sm m-2 border-none rounded h-[60px] md:w-[99%] w-[95%] items-center flex p-2 text-xl bg-gradient-to-l from-amber-100 to-yellow-100 justify-between">
             Notes
             <button onClick={menuClick}>
               <img
@@ -71,18 +77,19 @@ function App() {
             {isMenuOpen && (
               <div className="overflow-auto absolute mt-4 top-[70px] right-0 h-[300px] w-[250px] bg-gradient-to-r from-amber-200 to-yellow-300 z-10 rounded-xl p-2 shadow-xl">
                 <ul>
+                  <button onClick={()=>newNote()} className="hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-[115%] active:scale-95 border rounded-[50px] items-center flex justify-center border-white/50 p-2 m-2 bg-gradient-to-r from-amber-100 to-yellow-300">New Note</button>
                   {AllNotes.map((titleList) => (
                     <li
                       onClick={() => getNote(titleList.id)}
                       key={titleList.id}
-                      className="flex justify-between items-center p-2 hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-105 rounded-xl"
+                      className="m-1  border-none active:scale-95 flex justify-between items-center p-2 hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-105 rounded-xl bg-gradient-to-r from-amber-100 to-yellow-300"
                     >
-                      <p className="truncate overflow-hidden whitespace-nowrap w-[180px] ">
+                      <p className="text-black/75 truncate overflow-hidden whitespace-nowrap w-[180px] ">
                         {titleList.title}
                       </p>
 
                       <button
-                        className="hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-[115%]" onClick={() => removeNote(titleList.id)}>
+                        className="transition-all duration-300 rounded-xl  active:scale-95 hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-[110%]" onClick={() => removeNote(titleList.id)}>
                         x
                       </button>
                     </li>
@@ -93,20 +100,20 @@ function App() {
           </div>
 
           {/* Input section */}
-          <div className="w-[90%] flex justify-between">
+          <div className="w-[90%] flex justify-between p-2">
             <input
               value={title}
               onChange={(e) => {
                 setTitle(e.target.value);
               }}
               type="text"
-              className="hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 focus:outline-none ml-3 mt-5 border-none flex justify-center items-center h-[50px] rounded-xl p-2 bg-gradient-to-r from-amber-50 to-yellow-100 hover:scale-105"
+              className="text-lg text-black/75 border-none  hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 focus:outline-none ml-[10px] mt-5  flex justify-center items-center h-[50px] md:w-[300px] w-[200px] rounded-xl p-2 bg-gradient-to-r from-amber-50 to-yellow-100 hover:scale-105"
               placeholder="title"
             />
 
-            <div className="flex items-center mr-3 mt-2">
+            <div className="flex items-center md:mr-3 mr-[-20px] md:mt-2 mt-4">
               <button
-                className="mr-8 h-[40px] w-[80px] border-none bg-gradient-to-r from-amber-200 to-yellow-400 rounded-xl hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-105"
+                className="text-black/70 md:mr-8  h-[40px] md:w-[80px]  w-[90px] border-none bg-gradient-to-r from-amber-200 to-yellow-400 rounded-xl hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-105 active:scale-95"
                 onClick={saveNote}
               >
                 save
@@ -116,7 +123,7 @@ function App() {
 
           {/* Textarea */}
           <div className="flex justify-center items-center">
-            <div className="h-[550px] w-[90%] flex">
+            <div className="h-[550px] w-[99%] flex items-center justify-center">
               <textarea
                 type="text"
                 value={content}
@@ -124,7 +131,7 @@ function App() {
                   setContent(e.target.value);
                 }}
                 placeholder="type something...."
-                className="focus:outline-none ml-3 mt-5  w-[90%] h-[90%] rounded-xl p-4 bg-gradient-to-r from-amber-50 to-yellow-100 hover:scale-[102%] hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 "
+                className="border-none focus:outline-none  mt-5  w-[90%] h-[90%] rounded-xl p-4 bg-gradient-to-r from-amber-50 to-yellow-100 hover:scale-[102%] hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 text-black/75 "
               />
             </div>
           </div>
