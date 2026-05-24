08e164a9764e343febdeda90a8c2256ca1fe4f7b
ahirwarsatyam169 <ahirwarsatyam169@gmail.com>
2026-05-24 08:36:25 +0530

    Initialize notes from local storage and update saving
    Refactor App component to initialize notes from local storage and save notes to local storage on change.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import menu from "./assets/hamburgermenu.png";
 import React from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [AllNotes, setAllNotes] = useState([]);
+  const [AllNotes, setAllNotes] = useState(() => {
+    const savednote = localStorage.getItem("notes");
+    return savednote ? JSON.parse(savednote) : [];
+  });
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [isMenuOpen, setisMenuOpen] = useState(false);
+
+  //Saving notes to Local Storage
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(AllNotes));
+  }, [AllNotes]);
 
   // Saving note
   const saveNote = () => {
@@ -36,14 +45,6 @@ function App() {
     setAllNotes(fileteredNotes);
   };
 
-  //Deleting Notes
-  const DelNote = (id) => {
-    const deletedNote = AllNotes.find((dNOte) => dNOte.id === id);
-    setTitle("");
-    setContent("");
-    setTitleList("");
-  };
-
   //RETRIEVING THE NOTE FROM MENU
   const getNote = (id) => {
     const foundNote = AllNotes.find((gNote) => gNote.id === id);
@@ -55,12 +56,16 @@ function App() {
   return (
     <>
       <div className="flex justify-center h-screen w-screen items-center bg-gradient-to-r from-amber-100 to-amber-100">
-        <div className="shadow-xl border border-sm rounded-xl w-[90%] h-[90%] justify-center bg-gradient-to-r from-amber-200 to-yellow-200 relative">
+        <div className="shadow-xl border border-sm  w-[90%] h-[90%] justify-center bg-gradient-to-r from-amber-200 to-yellow-200 relative">
           {/* Header */}
           <div className="shadow-sm ml-2 mt-2 border-none rounded h-[60px] w-[99%] items-center flex p-2 text-xl bg-gradient-to-l from-amber-100 to-yellow-100 justify-between">
             Notes
             <button onClick={menuClick}>
-              <img className="h-[20px]" src={menu} alt="menu" />
+              <img
+                className="h-[20px] hover:scale-[110%] hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300"
+                src={menu}
+                alt="menu"
+              />
             </button>
             {/* Menu */}
             {isMenuOpen && (
@@ -70,13 +75,14 @@ function App() {
                     <li
                       onClick={() => getNote(titleList.id)}
                       key={titleList.id}
-                      className="flex justify-between items-center p-2"
+                      className="flex justify-between items-center p-2 hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-105 rounded-xl"
                     >
-                      <p className="truncate overflow-hidden whitespace-nowrap w-[180px]">
+                      <p className="truncate overflow-hidden whitespace-nowrap w-[180px] ">
                         {titleList.title}
                       </p>
 
-                      <button onClick={() => removeNote(titleList.id)}>
+                      <button
+                        className="hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-[115%]" onClick={() => removeNote(titleList.id)}>
                         x
                       </button>
                     </li>
@@ -94,13 +100,13 @@ function App() {
                 setTitle(e.target.value);
               }}
               type="text"
-              className="focus:outline-none focus:border-blue-500 ml-3 mt-5 border-none flex justify-center items-center h-[50px] rounded-xl p-2 bg-gradient-to-r from-amber-50 to-yellow-100"
+              className="hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 focus:outline-none ml-3 mt-5 border-none flex justify-center items-center h-[50px] rounded-xl p-2 bg-gradient-to-r from-amber-50 to-yellow-100 hover:scale-105"
               placeholder="title"
             />
 
             <div className="flex items-center mr-3 mt-2">
               <button
-                className="mr-8 h-[40px] w-[80px] border-none bg-gradient-to-r from-amber-200 to-yellow-400 rounded-xl"
+                className="mr-8 h-[40px] w-[80px] border-none bg-gradient-to-r from-amber-200 to-yellow-400 rounded-xl hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 hover:scale-105"
                 onClick={saveNote}
               >
                 save
@@ -118,7 +124,7 @@ function App() {
                   setContent(e.target.value);
                 }}
                 placeholder="type something...."
-                className="focus:outline-none ml-3 mt-5 border-none w-[90%] h-[90%] rounded-xl p-4 bg-gradient-to-r from-amber-50 to-yellow-100"
+                className="focus:outline-none ml-3 mt-5  w-[90%] h-[90%] rounded-xl p-4 bg-gradient-to-r from-amber-50 to-yellow-100 hover:scale-[102%] hover:shadow-xl hover:shadow-[0px_0px_40px_rgba(255,255,255,0.5)] transition-all duration-300 "
               />
             </div>
           </div>
